16954b9b9431f3b01a792759e6487938152dd900
Courtney White <Candcsoontobewhite@gmail.com>
2026-06-27 17:08:30 -0500

    Initial C. E. White Labs setup

---
+++ BEACON/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Beacon",
+    page_icon="💖",
+    layout="wide"
+)
+
+st.title("Beacon")
+st.subheader("Helping nonprofit leaders see the whole picture.")
+
+st.write("""
+Welcome to Beacon.
+
+This project is being built as part of C. E. White Labs.
+
+Every number represents a person.
+Every chart represents a story.
+""")
